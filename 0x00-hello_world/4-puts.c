84b707000c56b131843958274075d8776a7aedec
oussamaelhay <eloussamahayboubi@gmail.com>
2023-03-16 03:14:48 -0700

    ike building

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	    char str[] = "Programming is like building a multilingual puzzle";
-	        puts(str);
-		    return 0;
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
