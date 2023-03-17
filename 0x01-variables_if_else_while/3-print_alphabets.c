fbfe8404865a4ddc8edf43e0a0a953cc9963ea27
oussamaelhay <eloussamahayboubi@gmail.com>
2023-03-17 04:23:29 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
+ * main - Entry point
  *
  *
  *Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
-		char c;
+	char c;
 		for (c = 'a' ; c <= 'z' ; c++)
-					{
-						putchar(c);
-									}
-				for (c = 'A' ; c <= 'Z' ; c++)
-						{
-									putchar(c);
-						}
-					putchar('\n');
-					return (0);
+	{
+		putchar(c);
+	}
+		for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
