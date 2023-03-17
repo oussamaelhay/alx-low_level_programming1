77e0918613c86151ecf12e295b9cbaae4ac4e1b0
oussamaelhay <eloussamahayboubi@gmail.com>
2023-03-17 04:27:53 -0700

    I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
 /**
- *  * main - Prints the alphabet.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *main - Entry point
+ *
+ *
+ *Return: Always 0 (Sucess)
+ */
+
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-	for (i = 0; i < 26; i++)
-		{	
-			putchar(alp[i]);
-					}
+	char c;
+		for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
